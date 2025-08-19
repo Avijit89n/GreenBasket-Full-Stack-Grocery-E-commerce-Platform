7978850a2ecdbc7d78c5db4e95e7d6709de66712
Avijit89n <avijit89n@gmail.com>
2025-08-19 21:45:46 +0530

    add

--- Client/src/components/Common/CheckUserIsAuth.jsx
+++ Client/src/components/Common/CheckUserIsAuth.jsx
@@ -3,7 +3,6 @@ import { Navigate, useLocation } from 'react-router-dom';
 
 function CheckUserIsAuth({ isAuthenticated, user, children }) {
     const location = useLocation();
- 
     if (!isAuthenticated && !(location.pathname.includes('login') ||
         location.pathname.includes('home') ||
         location.pathname.includes('lists') ||
